3fb03aa4505833993e1e58989b8e6f60269ca64d
Jim Hutson <jhutson@idgcsmb.com>
2015-08-03 11:57:00 -0700

    move site from godaddy

---
+++ js/config.js
@@ -0,0 +1,17 @@
+boomerang.factory('Config', function () {
+    return {
+        //modify these
+        'name'          : 'GDG Modesto',
+        'id'            : '115831812831073891815',
+        'google_api'    : 'AIzaSyCvTT14LTkd8eNAKJjdJGmyqKZbYDcOcfM',
+        'pwa_id'        : '5833825016236017793', //picasa web album id, must belong to google+ id above
+        'cover' : {
+            title : 'Google Developer Groups Modesto',
+            subtitle : 'Using technology to improve our community.',
+//            button : {
+//                text : 'Register',
+//                url : 'http://gwtcreate.com/'
+//            }
+        }
+    };
+});
